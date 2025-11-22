4871e96503c7ccafa3cd134d084a60b263f6787f
Eng-YasminKotb <122429943+Eng-YasminKotb@users.noreply.github.com>
2025-11-22 14:59:29 +0200

    Add files via upload

---
+++ 01-Singleton /ThreadSafe-Singleton/Logger.java
@@ -0,0 +1,29 @@
+package Singleton.ThreadSafe;
+
+public class Logger {
+
+    private static volatile Logger instance;
+
+    private Logger() {
+        System.out.println("📝 Logger initialized...");
+    }
+
+    public static Logger getInstance() {
+
+        if (instance == null) {
+            synchronized (Logger.class) {
+                if (instance == null) {
+                    instance = new Logger();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("[LOG] " + message);
+    }
+}
+
+
